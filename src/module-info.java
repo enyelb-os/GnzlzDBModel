cea51064582ead1e1a648c6fbc79700b2319e6d7
enyelb-os <enyelb.gnzlz@gmail.com>
2023-11-04 21:28:31 -0400

    update

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module tools.gnzlz.database {
+    requires java.sql;
+
+    exports tools.gnzlz.database.autocode;
+    exports tools.gnzlz.database.autocode.model;
+    exports tools.gnzlz.database.model;
+    exports tools.gnzlz.database.model.interfaces;
+    exports tools.gnzlz.database.query.model;
+    exports tools.gnzlz.database.properties;
+}
